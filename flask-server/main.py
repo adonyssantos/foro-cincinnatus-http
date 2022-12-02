cd22697d51adb473f4ecdddf2d574495968ff330
Adonys Santos <adonyssantosjimenez@gmail.com>
2022-12-02 12:44:26 -0400

    Refactor flask server

--- flask-server/main.py
+++ flask-server/main.py
@@ -3,25 +3,28 @@ from flask_cors import CORS
 from db import Tasks
 
 app = Flask(__name__)
-CORS(app) # TODO: fix CORS
+CORS(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['CORS_HEADERS'] = 'Content-Type: application/json'
 
-@app.route('/tasks', methods=['GET', 'POST'])
-def tasks():
-  if request.method == 'GET':
-    return jsonify(Tasks['read']())
-  elif request.method == 'POST':
-    task = request.get_json()
-    tasks = Tasks['create'](task)
-    return jsonify(tasks)
-    
-    
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  tasks = Tasks['read']()
+
+  return jsonify(tasks)
+
 @app.route('/tasks/<string:id>', methods=['GET'])
-def task(id):
-  if request.method == 'GET':
-    task = Tasks['read_one'](id)
-    return jsonify(task)
+def get_task(id):
+  task = Tasks['read_one'](id)
 
+  return jsonify(task)
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  task = request.get_json()
+  tasks = Tasks['create'](task)
+
+  return jsonify(tasks)   
+    
 if __name__ == '__main__':
   app.run(debug=True, port=5050)
